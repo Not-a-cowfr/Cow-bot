bbb12f6419945a5aa7f2bd203975d5fa85feb8b6
not-a-cow <kly3333aswd@gmail.com>
2024-11-02 02:10:19 -0700

    change unimportant things

--- src/main.rs
+++ src/main.rs
@@ -25,11 +25,11 @@ async fn on_error(error: poise::FrameworkError<'_, Data, Error>) {
     match error {
         poise::FrameworkError::Setup { error, .. } => panic!("Failed to start bot: {:?}", error),
         poise::FrameworkError::Command { error, ctx, .. } => {
-            println!("Error in command `{}`: {:?}", ctx.command().name, error,);
+            println!("[ERROR] in command `{}`: {:?}", ctx.command().name, error,);
         }
         error => {
             if let Err(e) = poise::builtins::on_error(error).await {
-                println!("Error while handling error: {}", e)
+                println!("[ERROR] while handling error: {}", e)
             }
         }
     }
@@ -42,9 +42,9 @@ async fn main() {
     // FrameworkOptions contains all of poise's configuration option in one struct
     // Every option can be omitted to use its default value
     let options = poise::FrameworkOptions {
-        commands: vec![check_player::get_mojang_info()], // Register the commands
+        commands: vec![check_player::get_mojang_info()],                       // Register the commands
         prefix_options: poise::PrefixFrameworkOptions {
-            prefix: Some(".".into()),
+            prefix: Some(";".into()),                                                       // The prefix for all commands
             edit_tracker: Some(Arc::new(poise::EditTracker::for_timespan(
                 Duration::from_secs(3600),
             ))),
@@ -55,13 +55,13 @@ async fn main() {
         // This code is run before every command
         pre_command: |ctx| {
             Box::pin(async move {
-                println!("Executing command {}...", ctx.command().qualified_name);
+                println!("[COMMAND] started {}", ctx.command().qualified_name);
             })
         },
         // This code is run after a command if it was successful (returned Ok)
         post_command: |ctx| {
             Box::pin(async move {
-                println!("Executed command {}!", ctx.command().qualified_name);
+                println!("[COMMAND] completed {}", ctx.command().qualified_name);
             })
         },
         // Every command invocation must pass this check to continue execution
@@ -79,7 +79,7 @@ async fn main() {
         event_handler: |_ctx, event, _framework, _data| {
             Box::pin(async move {
                 println!(
-                    "Got an event in event handler: {:?}",
+                    "[EVENT HANDLER] {:?}",
                     event.snake_case_name()
                 );
                 Ok(())
@@ -102,7 +102,7 @@ async fn main() {
         .build();
 
     let token =
-        var("BOT_TOKEN").expect("Missing `BOT_TOKEN` env var, see README for more information.");
+        var("BOT_TOKEN").expect("[ERROR] Missing `BOT_TOKEN` env var, please include this in the environment variables");
     let intents =
         serenity::GatewayIntents::non_privileged() | serenity::GatewayIntents::MESSAGE_CONTENT;
 
